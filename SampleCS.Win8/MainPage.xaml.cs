71e31c8ffeaa55ffb72d76b2ee9d2584406571e3
jbienzms <jbienzms@hotmail.com>
2014-01-16 17:55:38 -0600

    Moved the Win8 version to a WinMD component
    Most code from RateReminder was moved to RateHelper, a plain old CLR
    class. This allowed access to these functions from JavaScript where an
    exception was being thrown when an attempt was made to instantiate the
    control. Sample.Win8 was changed to SampleCS.Win8 and a new
    SampleJS.Win8 was added to demonstrate how to use the functionality from
    a JavaScript application.

---
+++ SampleCS.Win8/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿#region License
+/******************************************************************************
+ * COPYRIGHT © MICROSOFT CORP. 
+ * MICROSOFT LIMITED PERMISSIVE LICENSE (MS-LPL)
+ * This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+ * 1. Definitions
+ * The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under U.S. copyright law.
+ * A "contribution" is the original software, or any additions or changes to the software.
+ * A "contributor" is any person that distributes its contribution under this license.
+ * "Licensed patents" are a contributor's patent claims that read directly on its contribution.
+ * 2. Grant of Rights
+ * (A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+ * (B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+ * 3. Conditions and Limitations
+ * (A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+ * (B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+ * (C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+ * (D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+ * (E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+ * (F) Platform Limitation- The licenses granted in sections 2(A) & 2(B) extend only to the software or derivative works that you create that run on a Microsoft Windows operating system product.
+ ******************************************************************************/
+#endregion // License
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace SampleCS.Win8
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class MainPage : SampleCS.Win8.Common.LayoutAwarePage
+    {
+        private const string ShownText = "Shown";
+        private const string NotShownText = "Not Shown";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+
+        private void RateReminder_TryReminderCompleted(object sender, AppPromo.RateReminderResult e)
+        {
+            var t = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    DaysLabel.Text = e.Days.ToString();
+                    RunsLabel.Text = e.Runs.ToString();
+                    ReminderLabel.Text = (e.ReminderShown ? ShownText : NotShownText);
+                    RatingLabel.Text = (e.RatingShown ? ShownText : NotShownText);
+                });
+        }
+
+        private void ResetCountersButton_Click(object sender, RoutedEventArgs e)
+        {
+            RateReminder.ResetCounters();
+            DaysLabel.Text = "0";
+            RunsLabel.Text = "0";
+            ReminderLabel.Text = NotShownText;
+            RatingLabel.Text = NotShownText;
+            ResetBlock.Text = "Counters Reset";
+        }
+    }
+}
---
+++ SampleJS.Win8/js/default.js
@@ -0,0 +1,66 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var rate = null;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+
+            args.setPromise(WinJS.UI.processAll().then(function ()
+            {
+                // Subscribe to the reset button
+                resetButton.addEventListener("click", resetClick, false);
+
+                // Create the reminder helper object
+                rate = AppPromo.RateHelper();
+
+                // Say we only want to wait 3 runs instead of the default 7
+                rate.runsBeforeReminder = 3;
+
+                // Try to show the reminder
+                rate.tryReminderAsync().then(function (result)
+                {
+                    // Show the results on the page
+                    numDays.innerText = result.days.toString();
+                    numRuns.innerText = result.runs.toString();
+                    reminderShown.innerText = result.reminderShown.toString();
+                    ratingShown.innerText = result.ratingShown.toString();
+                });
+            }));
+        }
+    };
+
+    function resetClick(mouseEvent)
+    {
+        rate.resetCounters();
+
+        numDays.innerText = "0";
+        numRuns.innerText = "0";
+        reminderShown.innerText = "";
+        ratingShown.innerText = "";
+    }
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
